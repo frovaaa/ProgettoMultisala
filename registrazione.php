d4324127538597a5f744ac2c6f881e9fd3b46e38
Paulpetta668 <48731892+Paulpetta668@users.noreply.github.com>
2022-05-06 11:35:48 +0200

    Update registrazione.php

--- registrazione.php
+++ registrazione.php
@@ -5,49 +5,92 @@
         <title>Registrazione</title>
     </head>
     <body>
-        <form method="post" action="">
+        <form method="post" action="" id="formBello">
             <div>
                 <label>Nome e Cognome</label>
-                <input name="nome" type="text" required>
-                <input name="cognome" type="text" required>
+                <input name="Nome" type="text" required>
+                <input name="Cognome" type="text" required>
             </div>
             <div>
                 <label>Email</label>
-                <input name="mail" type="email" required>
+                <input name="Mail" type="email" required>
             </div>
             <div>
                 <label>Username</label>
-                <input name="username" type="text" required>
+                <input name="Username" type="text" required>
+            </div>
+            <div>
+                <label>Password</label>
+                <input id="p1" name="Passwd" type="password" required>
+                <input id="p2" type="password" required>
+            </div>
+            <div>
+                <label>Cellulare</label>
+                <input name="Cellulare" type="text" required>
             </div>
 
             <div>
-                <label>Password</label>
-                <input id="p1" name="passwd" type="password" required>
-                <input id="p2" type="password" required>
+                <button type="button" id="btn">Registrati</button>
             </div>
         </form>
     </body>
     <script>
-        function event(){
+        let uguali = false;
+
+        const handler = function(e){
             let p1 = document.getElementById("p1");
             let p2 = document.getElementById("p2");
 
-            p1.addEventListener("input", controlla);
-            p1.addEventListener("propertychange", controlla);
-
-            p2.addEventListener("input", controlla);
-            p2.addEventListener("propertychange", controlla);
-        }
-
-        function controlla(){
-            let p1 = document.getElementById("p1");
-            let p2 = document.getElementById("p2");
+            if(verificaPass(p1, p2)){
+                console.log("non valida");
+                return;
+            }
 
             if(p1.value === p2.value){
-                console.log("sono ugualiiii");
+                uguali = true;
             }else{
-                console.log("Sono diverse");
+                uguali = false;
             }
+        }
+
+        const handlerBTN = function (e){
+            console.log("Bottone cliccato");
+
+            if(uguali){
+                let objForm = document.getElementById("formBello");
+
+                if(required()) objForm.submit();
+            }else{
+                console.log("le passwd non sono uguali quindi niente");
+            }
+        }
+
+        let p1 = document.getElementById("p1");
+        let p2 = document.getElementById("p2");
+
+        p1.addEventListener("input", handler);
+        p1.addEventListener("propertychange", handler);
+
+        p2.addEventListener("input", handler);
+        p2.addEventListener("propertychange", handler);
+
+        document.getElementById("btn").addEventListener("click", handlerBTN); //aggiungo l'eventHandler al bottone
+                                                                              //che gestirà il click
+
+        function required(){
+            let objs = document.getElementsByTagNameNS("input");
+
+            objs.forEach((item) =>{
+                
+            });
+
+            return true;
+        }
+
+        function verificaPass(p1, p2){ //ritorna false se la password è valida
+            if(p1.value.length < 8 || p2.value.length < 8) return true; //se la pass ha meno di 8 caratteri la dichiaro non valida
+
+            return false;
         }
     </script>
 </html>
