a4d8f00f8cc6c283be2a0c2a503fbfb0881649bd
dravid3x <39882540+dravid3x@users.noreply.github.com>
2022-05-11 13:55:56 +0200

    Update verificaInserisciFilm.php

--- Controllers/verificaInserisciFilm.php
+++ Controllers/verificaInserisciFilm.php
@@ -1,1 +1,12 @@
 <?php
+
+$titolo = $_POST['Titolo'];
+$trama = $_POST['Trama'];
+$copertina = $_POST['Copertina'];
+
+$nuovoFilm = new Film();
+$nuovoFilm->setTitolo($titolo);
+$nuovoFilm->setTrama($trama);
+$nuovoFilm->setCopertina($copertina);
+
+insertFilm($nuovoFilm);
