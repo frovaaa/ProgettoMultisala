0c77250754c58d20c39e416cd1c0a6a3262391fc
Paulpetta668 <48731892+Paulpetta668@users.noreply.github.com>
2022-05-13 10:17:03 +0200

    css homepage

--- homepage.php
+++ homepage.php
@@ -1,11 +1,7 @@
 <!doctype html>
 <html lang="it">
 <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-            crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="CSS/homepage.css">
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -23,6 +19,27 @@ if (isset($_SESSION['Utente'])) {
     echo "<h1>Benvenuto " . $utente->getUsername() . "</h1>";
 }
 ?>
+    <header>
+        <nav>
+            <div>
+                <div id="logo">
+                    <img src="Images/logo.png">
+                </div>
 
+                <div id="list">
+                    <ul>
+                        <li><a href="login.php">Login</a></li>
+                        <li><a href="registrazione.php">Registrati</a></li>
+                        <li><a href="#">Ciao</a></li>
+                        <li><a href="#">Ciao</a></li>
+                        <li><a href="#">Ciao</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+<main>
+
+</main>
 </body>
 </html>
