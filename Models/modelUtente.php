9db082bff4fef5d5b3cb911ee645af7fe004edd7
Paulpetta668 <48731892+Paulpetta668@users.noreply.github.com>
2022-05-18 13:07:33 +0200

    push

--- Models/modelUtente.php
+++ Models/modelUtente.php
@@ -1,7 +1,6 @@
 <?php
 
 class Utente {
-
   private $idUtente;
   private $idfRuolo;
   private $nome;
--- navBar.php
+++ navBar.php
@@ -5,12 +5,23 @@
                 <li id="logo">
                     <img src="Images/logo.png">
                 </li>
-                <li><a href="login.php">Login</a></li>
-                <li><a href="registrazione.php">Registrati</a></li>
+                <li><a href="login.php">Tua</a></li>
+                <li><a href="registrazione.php">Mamma</a></li>
                 <li><a href="#">Ciao</a></li>
                 <li><a href="#">Come</a></li>
                 <li><a href="#">va</a></li>
             </ul>
+        </div>
+        <div id="profile">
+            <div id="menuProfileWrapper">
+                <div id="logoProfile">
+                    <img src="">
+                </div>
+                <div id="listProfile">
+
+                </div>
+            </div>
+
         </div>
     </nav>
 </header>
